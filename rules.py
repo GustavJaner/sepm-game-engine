3637b25b7be129f91ac669d8b638f72a25fbb035
GustavJaner <gustav.janer@gmail.com>
2019-09-17 16:14:39 +0200

    [RULES] Add rule check for captured king

--- rules.py
+++ rules.py
@@ -9,16 +9,14 @@ def check_movement(board, board_size):
                     check_captured(board, board_size, row, col)
                 else:
                     check_king_escape(board, board_size,row,col)
+                    check_captured_king(board, board_size, row, col)
 
     return True, "valid move"
 
-def check_captured(board, board_size, row, col):
+def check_captured_marker(board, board_size, row, col):
     team = board[row][col].team
 
-    left  = None
-    right = None
-    top   = None
-    bot   = None
+    left, right, top, bot = None, None, None, None
 
     if (col-1 >= 0 and col+1 <= board_size["height"]-1):
         left  = board[row][col-1].team
@@ -35,14 +33,33 @@ def check_captured(board, board_size, row, col):
     if (top != None and bot!= None):
         if (top != team and bot != team):
             board[row][col].remove_marker()
-            
+
+
 def check_king_escape(board, board_size, row, col):
     if( (row == 0 and col == 0)
     or (row == board_size["width"]-1 and col == 0)
     or (row == 0 and col == board_size["height"]-1)
     or (row == board_size["width"]-1 and col == board_size["height"]-1)):
         print("King escaped, white win")
-        
+
+def check_captured_king(board, board_size, row, col):
+    team = "white"
+
+    left, right, top, bot = None, None, None, None
+
+    if (col-1 >= 0 and col+1 <= board_size["height"]-1):
+        left  = board[row][col-1].team
+        right = board[row][col+1].team
+
+    if (row-1 >= 0 and row+1 <= board_size["width"]-1):
+        top = board[row+1][col].team
+        bot = board[row-1][col].team
+
+    if (left != None and right!= None and top != None and bot != None):
+        if (left != team and right != team and top != team and bot != team):
+            board[row][col].remove_marker()
+            print("Black won!!")
+
 # 1. Check if move result in capturing a marker
 # --> remove that marker
 
