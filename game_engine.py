9c590c5899c94044f9a5c6fb8ec1e8ba001e3578
onmax <maximogarciamtnez@gmail.com>
2019-09-17 13:30:29 +0200

    moved cell class

--- game_engine.py
+++ game_engine.py
@@ -1,30 +1,5 @@
 from ui import UI
-
-
-class Cell():
-    def __init__(self, team, role, possible_target=False):
-        self.team = team
-        self.role = role
-        self.possible_target = possible_target
-
-    def __str__(self):
-        if self.role == "king":
-            char = "K"
-        elif self.team == "white":
-            char = "W"
-        elif self.team == "black":
-            char = "B"
-        elif self.possible_target:
-            char = "·"
-        else:
-            char = " "
-        return char
-
-    def is_piece(self):
-        return self.role in ["marker", "king"]
-
-    def set_possible_target(self, status):
-        self.possible_target = status
+from cell import Cell
 
 
 class GameEngine():
