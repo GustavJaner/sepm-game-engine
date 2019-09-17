9f3aab4cd24bd9e04f730f1b68055c59693f21f1
GustavJaner <gustav.janer@gmail.com>
2019-09-17 14:28:01 +0200

    [RULES] Init commit rules

--- rules.py
+++ rules.py
@@ -2,3 +2,11 @@
 def check_movement(board):
     # if invalid move -> return False, "reason"
     return True, "valid move"
+
+
+# 1. Check if move result in capturing a marker
+# --> remove that marker
+
+# 3 Win condition:
+# a: king in the corner -- White win
+# b: King is captureed -- Black win
