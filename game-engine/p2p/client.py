1344b89c7390950656afbd6d7ae42b148acf6b6f
GustavJaner <gustav.janer@gmail.com>
2019-10-11 13:11:22 +0200

    [P2P] improved some for simple communication and quitting session

--- game-engine/p2p/client.py
+++ game-engine/p2p/client.py
@@ -1,25 +1,32 @@
 import socket
 
-# Connect to server
+# Connect to host
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('0.0.0.0', 8080))
+print("< successfully joined a new online game >")
 
-# Send connection message to server
-msg = str.encode("I am CLIENT\n")
-client.send(msg)
+# Send connection message to host
+client.send(str.encode("I am CLIENT\n"))
 
-# Listen for verification message from server
-from_server = client.recv(4096).decode()
-print(from_server)
+# Listen for verification message from host
+from_host = client.recv(4096).decode()
+print("from_host:\n" + from_host)
 
+# Game loop
 while True:
-    # Listen for message from server
-    from_server = client.recv(4096).decode()
-    print(from_server)
+    # Listen for message from host
+    from_host = client.recv(4096).decode()
+    print("from_host:\n" + from_host)
+    if(from_host == "q"): # If host quit the current online game
+        print("< leaving game >")
+        break
 
-    # Send message to server
-    msg = str.encode(input("send message to server: "))
-    client.send(msg)
+    # Send message to host
+    msg = input("- send message to host (q to quite game): ")
+    client.send(str.encode(msg))
+    if(msg == "q"): # To quit the current online game
+        print("< leaving game >")
+        break
 
 
 client.close()
